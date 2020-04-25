b325dcee3f6f7dc4c94072a268f671ff9ee00fbc
arshad <arshadali.sw16@muetkhp.edu.pk>
2020-04-25 19:47:08 +0500

    just uploaded the project with required dependancies

---
+++ src/main/java/com/aliarshad/cms/main/CmsWithJspJstlApplication.java
@@ -0,0 +1,13 @@
+package com.aliarshad.cms.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsWithJspJstlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsWithJspJstlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliarshad/cms/main/CmsWithJspJstlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliarshad.cms.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsWithJspJstlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
